0464f5d4923932432638d2631a03a76cbc94fee9
= <zarkones.xena@gmail.com>
2025-06-24 00:29:55 +0200

    vuetify setup

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
     devtools: { enabled: true },
-    modules: ['@nuxt/test-utils']
+    build: {
+        transpile: ['vuetify'],
+    },
+    modules: [
+        '@nuxt/test-utils',
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({ autoImport: true }))
+            })
+        },
+    ],
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
+    },
 })
