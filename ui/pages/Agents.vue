266344475ffcc027691284b43e8992b34f6e6460
= <zarkones.xena@gmail.com>
2025-07-01 19:52:31 +0200

    wip

--- ui/pages/Agents.vue
+++ ui/pages/Agents.vue
@@ -49,7 +49,7 @@ const formatUnixNanoTime = (nanoStr: number) => {
         const year = date.getFullYear();
         const hours = date.getHours().toString().padStart(2, '0');
         const minutes = date.getMinutes().toString().padStart(2, '0');
-        return `${day}. ${month} ${year} / ${hours}:${minutes}`;
+        return `${day} ${month} ${year} / ${hours}:${minutes}`;
     }
 }
 
